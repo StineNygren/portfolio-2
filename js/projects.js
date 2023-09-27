0bd578b8055154d4c79ea14a3032abe12d295fa9
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-09-27 19:13:49 +0200

    started on other pages

---
+++ js/projects.js
@@ -0,0 +1,21 @@
+const projectApi = [
+  {
+    title: "GoldBrews",
+    img: ".././img./goldbrew.png",
+    date: "2023",
+    url: "https://vermillion-marshmallow-256c0b.netlify.app/",
+    description:
+      "Using HTML, CSS, and JavaScript, I built the website from scratch. To retrieve and display blog posts, I created a custom API within WordPress. This allowed me to integrate the blog posts through my page. The website is fully responsive, adapting to different screen sizes and devices.",
+    git: "https://github.com/StineNygren/nygren_stine_exam1",
+  },
+  {
+    title: "",
+    img: "",
+    date: "",
+    url: "",
+    description: "",
+    git: "",
+  },
+];
+
+export default projectApi;
