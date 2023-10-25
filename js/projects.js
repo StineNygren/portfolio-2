1106fcd29c3e399266dcffd22eca23601b0bac31
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-10-25 10:33:59 +0200

    projects added

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,9 @@
+const projectContainer = document.querySelector(".project-container");
+
 const projectApi = [
   {
     title: "GoldBrews",
-    img: ".././img./goldbrew.png",
+    img: "../img/goldbrew.png",
     date: "2023",
     url: "https://vermillion-marshmallow-256c0b.netlify.app/",
     description:
@@ -9,13 +11,49 @@ const projectApi = [
     git: "https://github.com/StineNygren/nygren_stine_exam1",
   },
   {
-    title: "",
-    img: "",
-    date: "",
-    url: "",
-    description: "",
-    git: "",
+    title: "Community Science Museum",
+    img: "../img/museum.png",
+    date: "2023",
+    url: "https://stupendous-stroopwafel-8bf13b.netlify.app/",
+    description:
+      "The entire website was built using HTML and CSS, ensuring all the necessary functionality while keeping it lightweight. The website is fully responsive, adapting to different screen sizes and devices.",
+    git: "https://github.com/StineNygren/stinenygren-semesterproject-1",
   },
+  {
+    title: "Rainy Days",
+    img: "../img/rainydays.png",
+    date: "2023",
+    url: "https://glittering-jelly-7e2d94.netlify.app/",
+    description:
+      "First built using HTML and CSS, I later added a WordPress API and JavaScript to update the site's capabilities. The website is fully responsive, adapting to different screen sizes and devices.",
+    git: "https://github.com/StineNygren/html-css-ca",
+  },
+
+  // {
+  //   title: "",
+  //   img: "",
+  //   date: "",
+  //   url: "",
+  //   description: "",
+  //   git: "",
+  // },
 ];
 
-export default projectApi;
+console.log(projectApi);
+
+projectApi.forEach((project) => {
+  projectContainer.innerHTML += `
+<div class="col-12 col-lg-6"> 
+<a href="${project.url}" class=" text-decoration-none  ">
+<h2 class="text-white" >${project.title}</h2>
+<div class="project-img">
+<img class="mb-3"  src="${project.img}">
+</div>
+<div class="d-flex flex-row justify-content-between">
+<p>${project.date}</p>
+<a class="git-icon" href="${project.git}"><i class="fa-brands fa-github"></i></a>
+</div>
+<P class="text-white">${project.description}</P>
+</a>
+</div>`;
+});
